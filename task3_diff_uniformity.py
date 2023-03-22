daadc36ba03a180083653b500139b6871e60d89b
Tobias.W.Thorsen <tobias.w.thorsen@student.uib.no>
2023-03-22 10:07:23 +0100

    started on task 3. Just initial setup of the script, nothing yet for computing differential uniformity

--- task3_diff_uniformity.py
+++ task3_diff_uniformity.py
@@ -1,2 +1,19 @@
 # TASK 3
 # Program to compute differential uniformity of a given (n,n)-function.
+import sys
+
+def read_lookup_table(lookup_table):
+    with open(f"sample_data/{lookup_table}", "r") as file:
+        return file.read()
+
+def compute_du():
+
+
+def main():
+    given_lookup_table = sys.argv[1]
+    lookup_table = read_lookup_table(given_lookup_table)
+    print(compute_du())
+
+
+if __name__=="__main__":
+    main()
