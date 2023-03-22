a7d1c2a8c749e69d63134dda10dc2711ea0b6a04
Tobias.W.Thorsen <tobias.w.thorsen@student.uib.no>
2023-03-22 13:24:05 +0100

    added verification function to the elgamal digital signature scheme

--- task2_elgamal.py
+++ task2_elgamal.py
@@ -1,8 +1,6 @@
 # Elgamal digital signature scheme
 from cipher import read_file, write_file, bits_to_bytes, bytes_to_bits
-import sys
-import math
-import random
+import math, random, sys
 
 def pick_ephemeral_key(public_key: int) -> int:
     temp_key = random.randint(1, public_key-2)
@@ -10,22 +8,27 @@ def pick_ephemeral_key(public_key: int) -> int:
         return temp_key
     return pick_ephemeral_key(public_key)
 
+def verification(beta: int, r: int, s: int, p: int, g: int, x: int) -> bool:
+    t = (pow(beta, r, p))*(pow(r, s, p)) % p
+    return t == pow(g, x, p)
+
 def main():
     # setup for Elgamal
     setup_arguments()
     # Separates the values from the parameters file
-    public_key = int(parameters[0] + "" + parameters[1])
+    p = int(parameters[0] + "" + parameters[1])
     generator = int(parameters[3])
     beta = int(parameters[5])
+    assert beta == pow(generator, private_key, p)
 
     # generates an ephemeral key:
-    ephemeral_key = pick_ephemeral_key(public_key)
+    ephemeral_key = pick_ephemeral_key(p)
 
     # compute r:
-    r = (generator**ephemeral_key) % public_key
-    # compute s: # ephemeral key her skal være inverse
-    eph_inverse = int(pow(ephemeral_key, -1, public_key-1))
-    s = ((message - (private_key*r) % public_key-1) * eph_inverse) % (public_key-1)
+    r = pow(generator, ephemeral_key, p)
+    # compute s:
+    eph_inverse = pow(ephemeral_key, -1, p-1)
+    s = ((message - (private_key*r)) * eph_inverse) % (p-1)
 
     # formatting the output
     output_message = f"{r}\n{s}"
@@ -38,6 +41,8 @@ def main():
 
     sample_output = read_file("sample_data/elg_output").decode("utf-8")
     print(sample_output)
+
+    print(verification(beta, r, s, p, generator, message))
 
 
 def setup_arguments():
