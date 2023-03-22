3d061412d55a3c766d466ac6ac4e4d9f88c02e06
Tobias.W.Thorsen <tobias.w.thorsen@student.uib.no>
2023-03-22 12:41:28 +0100

    task 3 about differential uniformity completed

--- task3_diff_uniformity.py
+++ task3_diff_uniformity.py
@@ -4,15 +4,34 @@ import sys
 
 def read_lookup_table(lookup_table):
     with open(f"sample_data/{lookup_table}", "r") as file:
-        return file.read()
+        return file.readlines()
 
-def compute_du():
+
+def compute_du(dict_ff: dict) -> int:
+    differential_uniformity = 0
+    for a in dict_ff:
+        if a != 000000:
+            for b in dict_ff:
+                numberOfPairs = 0
+                for x in dict_ff:
+                    if dict_ff[x] ^ dict_ff[x^a] == b:
+                        numberOfPairs += 1
+                if numberOfPairs > differential_uniformity:
+                    differential_uniformity = numberOfPairs
+    return differential_uniformity
 
 
 def main():
     given_lookup_table = sys.argv[1]
     lookup_table = read_lookup_table(given_lookup_table)
-    print(compute_du())
+
+    dict_ff = {}
+    for line in lookup_table:
+        num_x = int(line[0:6], 2) # keep it in binary format
+        num_Fx = int(line[8:14], 2)
+        dict_ff[num_x] = num_Fx
+
+    print(compute_du(dict_ff))
 
 
 if __name__=="__main__":
